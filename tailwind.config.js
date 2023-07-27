b3283b6dd1bbcc13f752a4ae89c2763cb4bf3fe8
Sixian Li <lisixian579@gmail.com>
2023-07-27 17:07:03 +0800

    fix: add index.html to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './index.html'],
   theme: {
     extend: {},
   },
