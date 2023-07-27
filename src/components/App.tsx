618e7ffee25e85b09a909eca53f2a5dbc5a3289d
Sixian Li <lisixian579@gmail.com>
2023-07-27 22:48:34 +0800

    feat: integrate audio

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,12 @@
-import { useCallback, useState } from 'react'
+import {
+  motion,
+  useMotionTemplate,
+  useMotionValueEvent,
+  useSpring,
+  useTransform,
+} from 'framer-motion'
+import { useCallback, useRef, useState } from 'react'
+import ReactHowler from 'react-howler'
 
 // for iOS 13+
 interface DeviceMotionEventiOS extends DeviceMotionEvent {
@@ -8,6 +16,25 @@ interface DeviceMotionEventiOS extends DeviceMotionEvent {
 function App() {
   const [motionEvent, setMotionEvent] = useState<DeviceMotionEvent>()
   const [isRunning, setIsRunning] = useState(false)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const howlerRef = useRef<ReactHowler>(null)
+
+  const volume = useSpring(0.1, { bounce: 0, duration: 8000 })
+  const playbackRate = useTransform(volume, [0, 1], [0.5, 5])
+
+  const sliderWidth = useTransform(volume, [0, 1], [0, 100])
+  const sliderHeight = useTransform(volume, [0, 0.1], [60, 100])
+
+  const blurRadius = useTransform(volume, [0, 1], [25, 0])
+
+  useMotionValueEvent(volume, 'change', latest => {
+    latest
+  })
+
+  useMotionValueEvent(playbackRate, 'change', latest => {
+    latest
+  })
 
   const handleMotion = useCallback((event: DeviceMotionEvent) => {
     setMotionEvent(event)
@@ -22,7 +49,10 @@ function App() {
             filterUnits="userSpaceOnUse"
             colorInterpolationFilters="sRGB"
           >
-            <feGaussianBlur stdDeviation="0" edgeMode="duplicate" />
+            <motion.feGaussianBlur
+              stdDeviation={blurRadius}
+              edgeMode="duplicate"
+            />
             <feComponentTransfer>
               <feFuncA type="discrete" tableValues="1 1" />
             </feComponentTransfer>
@@ -31,7 +61,45 @@ function App() {
       </svg>
       <div className="flex h-screen items-center justify-center text-white before:absolute before:inset-0 before:-z-10 before:h-full before:w-full before:bg-[url('/bg.jpg')] before:bg-cover before:content-[''] before:[filter:url(#blurFilter)]">
         <div>
+          <div className="relative flex h-11 w-80 items-center justify-start overflow-hidden rounded-full bg-stone-400 p-1">
+            <motion.div
+              className="h-full w-full overflow-hidden rounded-full bg-stone-800"
+              style={{
+                width: useMotionTemplate`${sliderWidth}%`,
+                height: useMotionTemplate`${sliderHeight}%`,
+              }}
+            ></motion.div>
+          </div>
+
+          <ReactHowler
+            src="/coffee-bean-sound.mp3"
+            playing={isPlaying}
+            loop
+            ref={howlerRef}
+          />
+
+          <div className="mt-4 flex items-center justify-center">
+            <button onClick={() => setIsPlaying(!isPlaying)}>Play</button>
+            <button
+              className="border p-3"
+              onClick={() => {
+                volume.set(1)
+              }}
+            >
+              +
+            </button>
+            <button
+              className="border p-3"
+              onClick={() => {
+                volume.set(0)
+              }}
+            >
+              -
+            </button>
+          </div>
+
           <button
+            className="hidden"
             onClick={async () => {
               if (isRunning) {
                 window.removeEventListener('devicemotion', handleMotion)
