fdf8ec5d88bd271cdb13d4309456af32fd41acda
Sixian Li <lisixian579@gmail.com>
2023-07-29 12:52:31 +0800

    feat(hook): use-stoppable-time

---
+++ src/hooks/use-stoppable-time.ts
@@ -0,0 +1,42 @@
+import {
+  useAnimationFrame,
+  MotionValue,
+  useMotionValue,
+} from 'framer-motion'
+import { useState } from 'react'
+
+export type UseStoppableTime = {
+  reset(): void
+  stop(): void
+  start(): void
+  val: MotionValue<number>
+}
+
+export function useStoppableTime(): UseStoppableTime {
+  const [isRunning, setIsRunning] = useState(false)
+  const t = useMotionValue(0)
+
+  useAnimationFrame((_, delta) => {
+    if (isRunning) {
+      t.set(t.get() + delta)
+    }
+  })
+  const reset = () => {
+    t.set(0)
+  }
+
+  const stop = () => {
+    setIsRunning(false)
+  }
+
+  const start = () => {
+    setIsRunning(true)
+  }
+
+  return {
+    reset,
+    stop,
+    start,
+    val: t,
+  }
+}
