3dbd3efa0d7b5832b5c6101c05c2f663df61518c
Sixian Li <lisixian579@gmail.com>
2023-07-29 23:38:58 +0800

    feat: add drag rotate

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import {
   motion,
   useMotionTemplate,
+  useMotionValue,
   useMotionValueEvent,
   useTransform,
 } from 'framer-motion'
@@ -15,7 +16,7 @@ import { useStoppableTime } from 'hooks/use-stoppable-time'
 import IconHearing from 'icon-hearing'
 import IconPhoneRotate from 'icon-phone-rotate'
 import { IconVolumeDown, IconVolumeUp } from 'icon-volume'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 function App() {
   const initialVolume = 0.5
@@ -38,6 +39,8 @@ function App() {
     },
   })
 
+  const barRef = useRef<HTMLDivElement | null>(null)
+
   const t = useStoppableTime()
   const gravityVolume = useGravityVolume(t, initialVolume)
   const volume = gravityVolume.volume
@@ -53,6 +56,8 @@ function App() {
     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
       navigator.userAgent
     )
+
+  const rotateAngle = useMotionValue(0)
 
   useMotionValueEvent(volume, 'change', latest => {
     if (sounds.isPlaying) {
@@ -125,7 +130,33 @@ function App() {
             </motion.span>
           </motion.button>
 
-          <div className="relative flex h-11 w-80 items-center justify-start overflow-hidden rounded-full bg-stone-400 p-1">
+          {/* Bar */}
+          <motion.div
+            ref={barRef}
+            style={{ rotateZ: useMotionTemplate`${rotateAngle}rad` }}
+            drag
+            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+            dragElastic={0}
+            dragMomentum={false}
+            onDrag={(e, info) => {
+              if (!barRef.current) return
+              const box = barRef.current.getBoundingClientRect()
+              const centerX = (box.left + box.right) / 2
+              const centerY = (box.top + box.bottom) / 2
+
+              const initialDragX = info.point.x - info.offset.x
+
+              const isLeftEnd = initialDragX < centerX
+
+              const angle = Math.atan2(
+                info.point.y - centerY,
+                info.point.x - centerX
+              )
+
+              rotateAngle.set(isLeftEnd ? angle - Math.PI : angle)
+            }}
+            className="relative flex h-11 w-80 items-center justify-start overflow-hidden rounded-full bg-stone-400 p-1"
+          >
             <motion.div
               className="h-full w-full overflow-hidden rounded-full bg-stone-800"
               style={{
@@ -133,7 +164,7 @@ function App() {
                 height: useMotionTemplate`${sliderHeight}%`,
               }}
             ></motion.div>
-          </div>
+          </motion.div>
 
           {status === 'granted' && (
             <p className="flex items-center gap-1 text-sm">
